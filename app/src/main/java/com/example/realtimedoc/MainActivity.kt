ae885227b626a4754c5812b85830600116c93ef4
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-12-08 14:58:54 +0530

    database refrence created,data WRITE IMPLEMENTED

--- app/src/main/java/com/example/realtimedoc/MainActivity.kt
+++ app/src/main/java/com/example/realtimedoc/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.realtimedoc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView= findViewById(R.id.textView)
+        val db= Firebase.database
+        val myref= db.getReference("message")
+
+
+        myref.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value= snapshot.value
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
+
     }
 }
